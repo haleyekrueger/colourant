82404c43a1d3659cd73663273a955730315342fe
Haley Krueger <67514633+haleyekrueger@users.noreply.github.com>
2022-12-09 11:33:19 -0800

    Fixed icon button for palette

--- colourant_frontend/src/components/PaletteSelectButton.js
+++ colourant_frontend/src/components/PaletteSelectButton.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const PaletteSelectButton = () => {
     return (
-        <button className="Palette-Select-Button"><i class="fa-thin fa-palette-2xl" style={{color: 'dodgerblue'}}></i>
+        <button class="btn"><span class="material-symbols-outlined">
+        palette
+        </span>
         </button>
     )
 }
